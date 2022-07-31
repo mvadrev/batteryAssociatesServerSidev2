ff3776d76607bce318e3771d7cdd7f875c136c79
Mukund Vadrev <mvadrev@gmail.com>
2022-08-01 06:00:05 +0900

    jwt auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { existingUserDTO } from 'src/user/dtos/existing-user.dto';
 import { NewUserDTO } from 'src/user/dtos/new-user.dto';
 import { UserDetails } from 'src/user/user-details.interface';
 import { AuthService } from './auth.service';
@@ -14,6 +15,12 @@ export class AuthController {
     registerUser(@Body() user: NewUserDTO): Promise<UserDetails| null> {
         // console.log("mmmm", user)
         return this.authService.registerUser(user);
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    loginUser(@Body() user: existingUserDTO): Promise<{token: string}| null> {
+        return this.authService.login(user);
     }
 
     @Get('register')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { NewUserDTO } from 'src/user/dtos/new-user.dto';
 import { UserDetails } from 'src/user/user-details.interface';
+import { existingUserDTO } from 'src/user/dtos/existing-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private jtwService: JwtService) {
 
     }
 
@@ -24,5 +26,28 @@ export class AuthService {
         const newUser = await this.userService.createUser(first_name, last_name, email, hashedPassword);
         console.log("hello", first_name, last_name, email, password)
         return this.userService._getUserDetails(newUser);
+    }
+
+    async doesPasswordMatchCheck(password: string, hashedPassword: string): Promise<boolean> {
+        return bcrypt.compare(password, hashedPassword);
+    }
+
+    async validateUSer(email: string, password: string): Promise<UserDetails | null> {
+        const user = await this.userService.findByEmail(email);
+        const doesUSerExist = !!user; 
+        if(!doesUSerExist) return null;
+        const doesPasswordMatch = await this.doesPasswordMatchCheck(password, user.password);
+        
+        if(!doesPasswordMatch) return null;
+        return this.userService._getUserDetails(user);
+    }
+
+    async login(existingUSer: existingUserDTO): Promise<{token: string} | null> {
+        const {email, password} = existingUSer;
+        const user = await this.validateUSer(email, password);
+        if(!user) return null;
+        const jwtToken = await this.jtwService.signAsync({user});
+        return {token: jwtToken};
+
     }
 }
